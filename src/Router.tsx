23ff90e07517b933204e555ff44ee48ab1eca2fa
Cottonwood <blog_geon@naver.com>
2022-03-21 21:02:24 +0900

    no message

---
+++ src/Router.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Child1 from "./components/Child1";
+import Child2 from "./components/Child2";
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          {/* <Route path="/:coinId" element={<Outlet />}> */}
+          {/* <Route index element={<Coin />} /> */}
+          <Route path="child1" element={<Child1 />} />
+          <Route path="child2" element={<Child2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
